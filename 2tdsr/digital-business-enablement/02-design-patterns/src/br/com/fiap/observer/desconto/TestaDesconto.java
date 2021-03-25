9ebbba873dd97296bf9f34a06e7464ac6149e014
dmaiolli <denysmaiolli@hotmail.com>
2021-03-25 14:25:45 -0300

    testando desconto, design pattern observer

--- 2tdsr/digital-business-enablement/02-design-patterns/src/br/com/fiap/observer/desconto/TestaDesconto.java
+++ 2tdsr/digital-business-enablement/02-design-patterns/src/br/com/fiap/observer/desconto/TestaDesconto.java
@@ -17,6 +17,24 @@ public class TestaDesconto {
 
 		System.out.println(desconto);
 		System.out.println(descontoo);
+		
+		for ( int i = 1 ; i <= 100; i++ ) { 
+			   if ( ( i %  3 ) == 0 ) {
+			      System.out.println("Fizz");
+			   }
+
+			   else if ( ( i % 5 ) == 0 ) {
+			     System.out.println("Buzz");
+			  }
+
+			  else if ( ( i % 3 ) == 0 && ( i % 5 ) == 0 ) { 
+			    System.out.println("FizzBuzz");
+			 }
+
+			 else { 
+			  System.out.println(i);
+			}
+			}
 
 	}
 }
